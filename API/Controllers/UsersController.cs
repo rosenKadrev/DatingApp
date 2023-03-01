5c85f3fbd75017d736504fa49c817fbd8fa2a8e3
Rosen Kadrev <Maria.Borshukova@inrix.com>
2023-03-01 08:41:57 +0200

    add register and login controllers in API

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -5,9 +5,8 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UsersController
+
+    public class UsersController: BaseApiController
     {
         private readonly DataContext _context;
         public UsersController(DataContext context)
