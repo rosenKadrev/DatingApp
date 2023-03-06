7d6f514e97d995781a57d88751f1ed4843173b9f
ivandimov <roro_901@abv.bg>
2023-03-06 17:42:08 +0200

    add relationship in database between user and photos and generate random data in database

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,5 +1,8 @@
 using API.Data;
+using API.DTOs;
 using API.Entities;
+using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -8,25 +11,29 @@ namespace API.Controllers
 {
     public class UsersController : BaseApiController
     {
-        private readonly DataContext _context;
-        public UsersController(DataContext context)
-        {
-            _context = context;
-        }
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
 
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        public UsersController(IUserRepository userRepository, IMapper mapper)
         {
-            var users = await _context.Users.ToListAsync();
-            return users;
+            _userRepository = userRepository;
+            _mapper = mapper;
         }
 
         [Authorize]
-        [HttpGet("{id}")]
-        public async Task<ActionResult<AppUser>> GetUser(int id)
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
         {
-            var user = await _context.Users.FindAsync(id);
-            return user;
+            var users = await _userRepository.GetMembersAsync();
+
+            return Ok(users);
+        }
+
+        [Authorize]
+        [HttpGet("{username}")]
+        public async Task<ActionResult<MemberDto>> GetUser(string username)
+        {
+            return await _userRepository.GetMemberAsync(username);
         }
     }
 }
