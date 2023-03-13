9413755bf5c595ed93776119cb8bee6f99a936b0
Rosen Kadrev <roro_901@abv.bg>
2023-03-13 17:32:22 +0200

    add message entities, repository,automapper profiles,controller and get messages from repo

---
+++ API/Data/MessageRepository.cs
@@ -0,0 +1,65 @@
+using API.DTOs;
+using API.Entities;
+using API.Helpers;
+using API.Interfaces;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+
+namespace API.Data
+{
+    public class MessageRepository : IMessageRepository
+    {
+        private readonly DataContext _context;
+        private readonly IMapper _mapper;
+
+        public MessageRepository(DataContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public void AddMessage(Message message)
+        {
+            _context.Messages.Add(message);
+        }
+
+        public void DeleteMessage(Message message)
+        {
+            _context.Messages.Remove(message);
+        }
+
+        public async Task<Message> GetMessage(int id)
+        {
+            return await _context.Messages.FindAsync(id);
+        }
+
+        public async Task<PagedList<MessageDto>> GetMessagesForUser(MessageParams messageParams)
+        {
+            var query = _context.Messages
+            .OrderByDescending(x => x.MessageSent)
+            .AsQueryable();
+            
+            query = messageParams.Container switch
+            {
+                "Inbox" => query.Where(u => u.RecipientUsername == messageParams.UserName),
+                "Outbox" => query.Where(u => u.SenderUsername == messageParams.UserName),
+                _ => query.Where(u => u.RecipientUsername == messageParams.UserName && u.DateRead == null)
+            };
+
+            var messages = query.ProjectTo<MessageDto>(_mapper.ConfigurationProvider);
+
+            return await PagedList<MessageDto>
+            .CreateAsync(messages, messageParams.PageNumber, messageParams.pageSize);
+        }
+
+        public Task<IEnumerable<MessageDto>> GetMessageThread(int currentUserId, int recipientId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> SaveAllAsync()
+        {
+            return await _context.SaveChangesAsync() > 0;
+        }
+    }
+}
--- client/src/app/Utils/interfaces.ts
+++ client/src/app/Utils/interfaces.ts
@@ -45,7 +45,7 @@ export class UserParams {
     minAge = 18;
     maxAge = 99;
     pageNumber = 1;
-    pageSize = 5;
+    pageSize = 12;
     orderBy = 'lastActive';
 
     constructor(user: User) {
